74440916dd8c5725e249cbaad4c2cb07868c4b36
Thiago Otoni <39462973+thiagootoni@users.noreply.github.com>
2022-07-08 00:04:38 -0300

    Entrega da Solução

---
+++ Node/index.js
@@ -0,0 +1,53 @@
+const e = require('express');
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+
+
+function insert() {
+    const mysql = require('mysql')
+    const connection = mysql.createConnection(config)
+    
+    
+    connection.query(sql)
+    connection.end()
+}
+
+
+app.get('/', (req,res) => {
+
+    const mysql = require('mysql')
+    const connection = mysql.createConnection(config)
+
+    try {
+        const insert = `INSERT INTO people(name) values('Thiago Full Cycle!')`;
+        connection.query(insert);
+
+        const selectAll = `SELECT * FROM PEOPLE;`
+        connection.query(selectAll, function (err, rows, fields) {
+            if (err) throw err;
+            let html = "<h1>Full Cycle Rocks!</h1>"
+            html += "<ul>"
+            rows.map(result => {
+                html += `<li>${result.name}</li>`
+            })
+            html += "</ul>"
+            res.send(html)
+        });
+    } catch (error) {
+        console.log(error);
+        res.send('<h3>Ocorreu um erro!</h3>');
+    }finally{
+        connection.end();
+    }    
+})
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # fullcycle-docker-desafio-nginx-node
+
+Repo com finalidade de entrega dos desafios de docker do módulo referente.
+
+Para rodar o desafio 2, "Nginx com Node.js", basta executar o docker compose que está em:
+
+```/desafio ngix - node - mysql/docker-compose.yaml```
+
+Após ter o arquivo em posse, basta:
+
+```docker-compose up -d```
